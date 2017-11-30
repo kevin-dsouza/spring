05dad35a84a386d3ab0431b2a3d19d89bde1e14b
Kevin Dsouza <kevin.dsouza@outlook.com>
2017-11-29 20:53:33 -0600

    First Commit

---
+++ src/main/java/com/znz/demo/DemoApplication.java
@@ -0,0 +1,31 @@
+package com.znz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+    
+    @RestController
+    class PatientController {
+    	
+    	@RequestMapping("/hello")
+    	public String getPatient() {
+    		return "Hello Kevin";
+    	}
+    }
+
+}
